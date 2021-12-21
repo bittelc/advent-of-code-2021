7ce02d3965474ac8bb6f16f36f574f6d18535ad1
Cole Bittel <cole.bittel@gmail.com>
2021-12-20 20:20:54 -0600

    complete with part 1

--- 4/main.go
+++ 4/main.go
@@ -9,7 +9,8 @@ import (
 	"strings"
 )
 
-var drawNumList []int = []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1}
+// var drawNumList []int = []int{7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1}
+var drawNumList []int = []int{4, 77, 78, 12, 91, 82, 48, 59, 28, 26, 34, 10, 71, 89, 54, 63, 66, 75, 15, 22, 39, 55, 83, 47, 81, 74, 2, 46, 25, 98, 29, 21, 85, 96, 3, 16, 60, 31, 99, 86, 52, 17, 69, 27, 73, 49, 95, 35, 9, 53, 64, 88, 37, 72, 92, 70, 5, 65, 79, 61, 38, 14, 7, 44, 43, 8, 42, 45, 23, 41, 57, 80, 51, 90, 84, 11, 93, 40, 50, 33, 56, 67, 68, 32, 6, 94, 97, 13, 87, 30, 18, 76, 36, 24, 19, 20, 1, 0, 58, 62}
 
 type space map[int]bool
 type row []space
@@ -25,19 +26,51 @@ func main() {
 		winner, crd := checkForRow(&crds)
 		if winner {
 			fmt.Printf("there's a row winner from card %+v containing %v\n", crd, drewNum)
+			sum := sumUnmarked(&crd)
+			fmt.Println("final answer:", sum*drewNum)
 			return
 		}
-		// winner, crd = checkForColumn(crds)
-		// if winner {
-		// 	fmt.Println("there's a column winner from card %v containing %v", crd, i)
-		// }
+		winner, crd = checkForColumn(&crds)
+		if winner {
+			fmt.Printf("there's a column winner from card %v containing %v\n", crd, drewNum)
+			sum := sumUnmarked(&crd)
+			fmt.Println("final answer:", sum*drewNum)
+			return
+		}
 	}
+}
+
+func sumUnmarked(crd *card) int {
+	sum := 0
+	for _, rw := range *crd {
+		for _, keyval := range rw {
+			for key, val := range keyval {
+				if val == false {
+					sum = sum + key
+				}
+			}
+		}
+	}
+	return sum
 }
 
 func checkForColumn(crds *cards) (bool, card) {
 	for _, crd := range *crds {
 		for i := 0; i <= 4; i++ {
-			keyval := 
+			consecutive := 0
+		col:
+			for _, rw := range crd {
+				el := rw[i]
+				for _, marked := range el {
+					if marked == false {
+						break col
+					}
+					consecutive++
+					if consecutive == 5 {
+						return true, crd
+					}
+				}
+			}
 		}
 	}
 	return false, nil
@@ -80,7 +113,7 @@ func draw(i int) int {
 
 func getInputs() cards {
 	crds := cards{}
-	file, err := os.Open("test_input.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
